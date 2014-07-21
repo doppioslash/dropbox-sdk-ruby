83f1b586c9e41114b4918ed96f19c941384754ee
Claudia Doppioslash <claudia.doppioslash@gmail.com>
2014-07-21 17:36:11 +0100

    Fix for jruby

--- lib/dropbox_sdk.rb
+++ lib/dropbox_sdk.rb
@@ -7,6 +7,26 @@ require 'base64'
 require 'securerandom'
 require 'pp'
 
+require 'open-uri'
+require 'net/https'
+
+module Net
+  class HTTP
+    alias_method :original_ciphers=, :ciphers=
+
+    def ciphers=(ciphs)
+      if Dropbox::TRUSTED_CERT_FILE.eql?(self.ca_file)
+        # special hack to fix a problem with dropbox gem 1.6.4 under JRuby 1.7.12
+        # dropbox sdk set ciphers not suitable for JRuby SSLContext, which would 
+        # cause exception ...
+        original_ciphers = nil
+      else
+        original_ciphers = ciphs
+      end
+    end
+  end
+end
+
 module Dropbox # :nodoc:
   API_SERVER = "api.dropbox.com"
   API_CONTENT_SERVER = "api-content.dropbox.com"
